76f39dc2a1098f7565109a6631bbe2b4aabe1e31
StudentofTATU <musogulomov123@gmail.com>
2023-03-25 11:58:20 +0500

    Computer Robot Player

---
+++ rock-paper-scissors-game/rock-paper-scissors-game/Services/Player/Computer.cs
@@ -0,0 +1,49 @@
+﻿using rock_paper_scissors_game.Models;
+using rock_paper_scissors_game.Services.Rule;
+using rock_paper_scissors_game.Services.Security;
+
+namespace rock_paper_scissors_game.Services.Player
+{
+    internal class Computer
+    {
+        public string[] Moves { set; get; }
+        public Move ComputerMove { get; set; }
+
+        SecurityService securityService;
+
+        public Computer(SecurityService securityService)
+        {
+            this.securityService = securityService;
+            ComputerMove = new Move();
+        }
+
+        public void Play()
+        {
+            if (MoveRule.IsMovesValid(Moves))
+            {
+                GenerateComputerMove();
+            }
+        }
+
+        private void GenerateComputerMove()
+        {
+            ComputerMove.Value = GetRandomMove();
+            ComputerMove.Key = GetKey();
+            ComputerMove.HMAC = GetHMAC(ComputerMove.Key, ComputerMove.Value);
+        }
+
+        public string GetRandomMove()
+        {
+            int number = new Random().Next(0, Moves.Length);
+
+            return Moves[number];
+        }
+
+        public string GetKey() =>
+            securityService.GenerateCryptographicallyKey();
+
+        public string GetHMAC(string key, string message) =>
+            securityService.GenerateHmacValue(key, message);
+
+    }
+}

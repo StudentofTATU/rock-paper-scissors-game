2a38392638e2833e8dd90c4c5be5b933b8a17c89
StudentofTATU <musogulomov123@gmail.com>
2023-03-25 11:57:32 +0500

    Create Move Model

---
+++ rock-paper-scissors-game/rock-paper-scissors-game/Models/Move.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rock_paper_scissors_game.Models
+{
+    public class Move
+    {
+        public string Value { get; set; }
+        public string HMAC { get; set; }
+        public string Key{ get; set; }
+    }
+}
